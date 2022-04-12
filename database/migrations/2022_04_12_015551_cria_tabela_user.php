d05c9842bb8cb2fe3287b1483c255a91f46ccf1d
Murilo Avila <murilo.avila068@gmail.com>
2022-04-11 23:28:08 -0300

    first commit

---
+++ database/migrations/2022_04_12_015551_cria_tabela_user.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 80);
+            $table->string('email', 120);
+            $table->string('password', 256);
+            $table->string('login', 50)->unique();
+            $table->string('email_verified_at', 120);
+            $table->rememberToken();                  // Each user has a remember token (to allow him or her to stay signed in
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
